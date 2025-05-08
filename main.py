f0ce50caa087ee877785d8531d46c0247b403797
Narain3108 <narainkarthik007@gmail.com>
2025-05-08 10:22:17 +0530

    temp1 backend

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ db = initialize_firebase()
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://interview-process-tau.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
